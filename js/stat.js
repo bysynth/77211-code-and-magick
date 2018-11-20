bc9a3acfe9599446bf2f19e8c45afa648a85867f
VK <belsynth@gmail.com>
2018-11-20 13:50:25 +0300

    Создал функцию renderText и добавил enum-объект Text c константами для создания текста

--- js/stat.js
+++ js/stat.js
@@ -10,12 +10,30 @@ var Cloud = {
   SHADOW: 'rgba(0, 0, 0, 0.7'
 };
 
+var Text = {
+  X: 120,
+  Y: 40,
+  OFFSET: 20,
+  FONT: '16px PT Mono',
+  COLOR: '#000000',
+  CONTENT: ['Ура вы победили!', 'Список результатов:']
+};
+
 var renderCloud = function (ctx, x, y, width, height, color) {
-  ctx.fillStyle = color;
+  ctx.fillStyle = color || '#000000';
   ctx.fillRect(x, y, width, height);
+};
+
+var renderText = function (ctx, color, font, text, x, y) {
+  ctx.fillStyle = color || '#000000';
+  ctx.font = font || '16px Arial';
+  ctx.fillText(text, x, y);
 };
 
 window.renderStatistics = function (ctx, names, times) {
   renderCloud(ctx, Cloud.X + Cloud.OFFSET, Cloud.Y + Cloud.OFFSET, Cloud.WIDTH, Cloud.HEIGHT, Cloud.SHADOW);
   renderCloud(ctx, Cloud.X, Cloud.Y, Cloud.WIDTH, Cloud.HEIGHT, Cloud.COLOR);
+
+  renderText(ctx, Text.COLOR, Text.FONT, Text.CONTENT[0], Text.X, Text.Y);
+  renderText(ctx, Text.COLOR, Text.FONT, Text.CONTENT[1], Text.X, Text.Y + Text.OFFSET);
 };
