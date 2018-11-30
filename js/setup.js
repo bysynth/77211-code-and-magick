221dc48c2ae84238ff6de8268bd29a75866d962d
VK <belsynth@gmail.com>
2018-11-30 17:13:19 +0300

    Написал функции обработчики событий для открытия-закрытия окна и установил слушатели событий для интерактивных элементов

--- js/setup.js
+++ js/setup.js
@@ -5,14 +5,55 @@ var RandomData = {
   SURNAMES: ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'],
   COATCOLORS: ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'],
   EYESCOLORS: ['black', 'red', 'blue', 'yellow', 'green'],
-  NUMBER: 4
+  COUNT: 4
 };
 
+var ESC_CODE = 27;
+var ENTER_CODE = 13;
+
+var setupOpen = document.querySelector('.setup-open');
 var userDialog = document.querySelector('.setup');
-userDialog.classList.remove('hidden');
+var setupClose = userDialog.querySelector('.setup-close');
+var usernameInput = userDialog.querySelector('.setup-user-name');
 
 var list = userDialog.querySelector('.setup-similar-list');
 var template = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
+
+// Вопрос - как быть с наименованием обработчиков, ведь одни и те же обработкичи я навешиваю как на клик, так и на нажатие кнопки. Например, openPopup стоит назвать setupOpenClickKeydownHandler? или onSetupOpenClickKeydown? Или оставить как есть?
+
+var openPopup = function () {
+  userDialog.classList.remove('hidden');
+  document.addEventListener('keydown', onPopupEscPress);
+};
+
+var closePopup = function () {
+  userDialog.classList.add('hidden');
+  document.removeEventListener('keydown', onPopupEscPress);
+};
+
+var onPopupEscPress = function (evt) {
+  var activeElement = document.activeElement;
+
+  if (evt.keyCode === ESC_CODE && usernameInput !== activeElement) {
+    closePopup();
+  }
+};
+
+setupOpen.addEventListener('click', openPopup);
+
+setupOpen.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_CODE) {
+    openPopup();
+  }
+});
+
+setupClose.addEventListener('click', closePopup);
+
+setupClose.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_CODE) {
+    closePopup();
+  }
+});
 
 var randomIndex = function (arr) {
   return Math.floor(Math.random() * arr.length);
@@ -65,7 +106,7 @@ var renderAllWizards = function (wizardsDataArray) {
   list.appendChild(fragment);
 };
 
-var wizardsData = generateWizardsDataArray(RandomData.NUMBER);
+var wizardsData = generateWizardsDataArray(RandomData.COUNT);
 
 renderAllWizards(wizardsData);
 
