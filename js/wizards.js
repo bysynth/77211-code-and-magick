10e98b67ef439412b867af26af4b884f680c864f
VK <belsynth@gmail.com>
2018-12-12 23:46:21 +0300

    Подключил в wizards.js загрузку данных и изменил функцию рендеринга всех волшебников

--- js/wizards.js
+++ js/wizards.js
@@ -5,55 +5,26 @@
   var list = window.dialog.querySelector('.setup-similar-list');
   var template = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
 
-  var generateWizardName = function (names, surnames) {
-    var optionNameSurname = names[window.utils.randomIndex(names)] + ' ' + surnames[window.utils.randomIndex(surnames)];
-    var optionSurnameName = surnames[window.utils.randomIndex(surnames)] + ' ' + names[window.utils.randomIndex(names)];
-
-    return Math.round(Math.random()) ? optionNameSurname : optionSurnameName;
-  };
-
-  var generateWizardData = function () {
-    var wizard = {};
-
-    wizard.name = generateWizardName(window.data.NAMES, window.data.SURNAMES);
-    wizard.coatColor = window.utils.chooseColor(window.data.COAT_COLORS);
-    wizard.eyesColor = window.utils.chooseColor(window.data.EYES_COLORS);
-
-    return wizard;
-  };
-
-  var generateWizardsDataArray = function (number) {
-    var arr = [];
-
-    for (var i = 0; i < number; i++) {
-      arr.push(generateWizardData());
-    }
-
-    return arr;
-  };
-
   var renderWizard = function (wizard) {
     var wizardElement = template.cloneNode(true);
 
     wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
-    wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
-    wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+    wizardElement.querySelector('.wizard-coat').style.fill = wizard.colorCoat;
+    wizardElement.querySelector('.wizard-eyes').style.fill = wizard.colorEyes;
 
     return wizardElement;
   };
 
   var renderAllWizards = function (wizardsDataArray) {
     var fragment = document.createDocumentFragment();
-
-    for (var i = 0; i < wizardsDataArray.length; i++) {
+    for (var i = 0; i < 4; i++) {
       fragment.appendChild(renderWizard(wizardsDataArray[i]));
     }
-
     list.appendChild(fragment);
+
+    window.dialog.querySelector('.setup-similar').classList.remove('hidden');
   };
 
-  renderAllWizards(generateWizardsDataArray(window.data.COUNT));
-
-  window.dialog.querySelector('.setup-similar').classList.remove('hidden');
+  window.backend.load(renderAllWizards, window.error);
 
 })();
