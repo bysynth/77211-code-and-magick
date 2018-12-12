58405c1d361757fb4c058b8be2674a83ac9412a1
VK <belsynth@gmail.com>
2018-12-12 23:44:33 +0300

    Написал функцию для рендеринга блока ошибок загрузки-отправки данных

---
+++ js/error.js
@@ -0,0 +1,19 @@
+'use strict';
+
+(function () {
+
+  var renderError = function (message) {
+    var node = document.createElement('div');
+    node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red;';
+    node.style.position = 'absolute';
+    node.style.left = 0;
+    node.style.right = 0;
+    node.style.fontSize = '30px';
+
+    node.textContent = message;
+    document.body.insertAdjacentElement('afterbegin', node);
+  };
+
+  window.error = renderError;
+
+})();
